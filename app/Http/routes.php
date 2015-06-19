05e5103663228b64a29fe2e2916a7c901317f216
Chandresh <chandresh989@gmail.com>
2015-06-19 18:36:08 +0530

    created app.blade.php and used bootstrap layout and navbar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/**
+ *  The Home Page
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
+
+
+/**
+ *  Authentication
+ */
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
