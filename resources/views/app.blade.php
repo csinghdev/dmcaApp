54df25ba87244fc69cdc58681310cadc4920b056
Chandresh <chandresh989@gmail.com>
2015-06-20 21:25:55 +0530

    npm install and used glup to include bootstrap and sass files into all.css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DMCA App</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/all.css">
 
 </head>
 <body>
