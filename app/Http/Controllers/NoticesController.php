529b7ffeff7b4c8fa4a6ec4d21ad51ea32b7bf0a
Chandresh <chandresh989@gmail.com>
2015-06-21 18:15:41 +0530

    created a template of letter and a confirm page to let user check and confirm the letter

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PrepareNoticeRequest;
 use App\Provider;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -43,11 +44,37 @@ class NoticesController extends Controller
         return view('notices.create', compact('providers'));
     }
 
-    public function confirm(PrepareNoticeRequest $request)
+    /**
+     * Ask the user to confirm the DMCA that will be delivered.
+     *
+     * @param PrepareNoticeRequest $request
+     * @param Guard $auth
+     * @return \Response
+     */
+    public function confirm(PrepareNoticeRequest $request, Guard $auth)
     {
-        return $request->all();
+        $template = $this->compleDmcaTemplate($request->all(), $auth);
+
+        return view('notices.confirm', compact('template'));
     }
 
+    /**
+     * compile the DMCA template from the form data.
+     *
+     * @param $data
+     * @param Guard $auth
+     * @return mixed
+     */
+    public function compleDmcaTemplate($data, Guard $auth)
+    {
+        $data = $data + [
+                'name' => $auth->user()->name,
+                'email' => $auth->user()->email,
+            ];
+
+        return view()->file(app_path('Http/Templates/dmca.blade.php'), $data);
+
+    }
     /**
      * Store a newly created resource in storage.
      *
---
+++ app/Http/Templates/dmca.blade.php
@@ -0,0 +1,20 @@
+I am the copyright owner of the content, "{{ $infringing_title }}" being infringed at:
+
+{{ $infringing_link }}
+
+You may find the original content at:
+
+{{ $original_link }}
+
+{{ $original_description }}
+
+This letter is official documentation under the provision of section 512(c) of the Digital Millenium Copyright Act ("DMCA)" to effect removal of the above reported infringements. I request that you immediately issue a cancellation message as specified in RFC 1036 for the specified postings and prevent the infringer, who is identified by its web address, from posting material to your server in the future. Please be advised that law requires you, as a service provider, to "expeditiously remove or disable access to" the infringing material upon receiving this notice. Noncompliance may result in a loss of immunity for liability under the DMCA.
+
+I have a good faith belief that the use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law. The information in the notification is accurate, and under penality of perjury, I am the owner, or an agent authorized to act on behalf of the owner, of an exclusive right that is allegedly infringed.
+
+Please send me at the address noted below a prompt response indicating the actions you have taken to resolve this matter.
+
+Sincerely,
+
+{{ $name }}
+{{ $email }}
